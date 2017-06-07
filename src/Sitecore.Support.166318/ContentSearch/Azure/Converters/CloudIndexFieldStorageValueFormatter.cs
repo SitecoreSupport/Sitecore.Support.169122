1aee4437b50190bc74b20cde0027814526481a17
DK\psk <psk@sitecore.net>
2017-06-07 13:25:04 -0400

    Fix for #162451 Search queries may use wrong syntax ignoring Azure Search schema settings

---
+++ src/Sitecore.Support.166318/ContentSearch/Azure/Converters/CloudIndexFieldStorageValueFormatter.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Sitecore.ContentSearch;
+using Sitecore.ContentSearch.Converters;
+using Sitecore.Diagnostics;
+
+namespace Sitecore.Support.ContentSearch.Azure.Converters
+{
+    public class CloudIndexFieldStorageValueFormatter : Sitecore.ContentSearch.Azure.Converters.CloudIndexFieldStorageValueFormatter
+    {
+        private static readonly FieldInfo searchIndexFieldInfo;
+        private static readonly MethodInfo convertToTypeMethodInfo;
+
+        static CloudIndexFieldStorageValueFormatter()
+        {
+            searchIndexFieldInfo = typeof(Sitecore.ContentSearch.Azure.Converters.CloudIndexFieldStorageValueFormatter).GetField(
+                "searchIndex", BindingFlags.Instance | BindingFlags.NonPublic);
+            convertToTypeMethodInfo = typeof(Sitecore.ContentSearch.Azure.Converters.CloudIndexFieldStorageValueFormatter).GetMethod(
+                "ConvertToType", BindingFlags.Instance | BindingFlags.NonPublic);
+        }
+        public override object FormatValueForIndexStorage(object value, string fieldName)
+        {
+            Assert.IsNotNullOrEmpty(fieldName, nameof(fieldName));
+
+            var result = value;
+
+            if (result == null)
+            {
+                return null;
+            }
+
+            var fieldSchema = ((CloudSearchProviderIndex)searchIndexFieldInfo.GetValue(this)).SchemaBuilder.GetSchema().GetFieldByCloudName(fieldName);
+            if (fieldSchema == null)
+            {
+                fieldSchema = ((CloudSearchProviderIndex)searchIndexFieldInfo.GetValue(this)).SearchService.Schema.GetFieldByCloudName(fieldName);
+            }
+            //Quick fix for computed fields
+            if (fieldSchema == null)
+            {
+                return value;
+            }
+
+            var cloudTypeMapper = ((CloudSearchProviderIndex)searchIndexFieldInfo.GetValue(this)).CloudConfiguration.CloudTypeMapper;
+            var fieldType = cloudTypeMapper.GetNativeType(fieldSchema.Type);
+
+            var context = new IndexFieldConverterContext(fieldName);
+
+            try
+            {
+                if (result is IIndexableId)
+                {
+                    result = this.FormatValueForIndexStorage(((IIndexableId)result).Value, fieldName);
+                }
+                else if (result is IIndexableUniqueId)
+                {
+                    result = this.FormatValueForIndexStorage(((IIndexableUniqueId)result).Value, fieldName);
+                }
+                else
+                {
+                    result = convertToTypeMethodInfo.Invoke(this, new object[] {result, fieldType, context});
+                }
+
+                if (result != null && !(result is string || fieldType.IsInstanceOfType(result) || (result is IEnumerable<string> && typeof(IEnumerable<string>).IsAssignableFrom(fieldType))))
+                {
+                    throw new InvalidCastException($"Converted value has type '{result.GetType()}', but '{fieldType}' is expected.");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new NotSupportedException($"Field '{fieldName}' with value '{value}' of type '{value.GetType()}' cannot be converted to type '{fieldType}' declared for the field in the schema.", ex);
+            }
+
+            return result;
+        }
+    }
+}
--- src/Sitecore.Support.166318/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.166318/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Sitecore.Support.166318")]
-[assembly: AssemblyProduct("Sitecore.Support.166318")]
+[assembly: AssemblyTitle("Sitecore.Support.166318.162451")]
+[assembly: AssemblyProduct("Sitecore.Support.166318.162451")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyDescription("Azure Search provider cannot handle queries that use conditions based DateTime objects")]
+[assembly: AssemblyDescription("Azure Search provider cannot handle queries that use conditions based DateTime objects. Search queries may use wrong syntax ignoring Azure Search schema settings.")]
 
