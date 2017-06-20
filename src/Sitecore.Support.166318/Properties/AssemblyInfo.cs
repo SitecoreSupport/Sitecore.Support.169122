a8590c474e7e9b9db0aabbef7204527698bd71b0
DK\psk <psk@sitecore.net>
2017-06-20 19:57:31 -0400

    Switch to cumulative patch apporach

--- src/Sitecore.Support.166318/Properties/AssemblyInfo.cs
+++ src/Sitecore.Support.166318/Properties/AssemblyInfo.cs
@@ -1,8 +1,8 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("Sitecore.Support.166318.162451")]
-[assembly: AssemblyProduct("Sitecore.Support.166318.162451")]
+[assembly: AssemblyTitle("Sitecore.Support.169122")]
+[assembly: AssemblyProduct("Sitecore.Support.169122")]
 [assembly: ComVisible(false)]
 [assembly: AssemblyDescription("Azure Search provider cannot handle queries that use conditions based DateTime objects. Search queries may use wrong syntax ignoring Azure Search schema settings.")]
 
