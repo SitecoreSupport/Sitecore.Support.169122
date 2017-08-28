c2d9be853f8634a6fbeb1fa884d0b1b4450733e1
DK\psk <psk@sitecore.net>
2017-08-28 17:55:07 -0400

    Merge commit db7a0670943b11bfae433cd046789a4618e6e8c6 from Sitecore.Support.158988  repository. Fix for 147550

--- src/Sitecore.Support.166318/ContentSearch/Azure/Query/LinqToCloudIndex.cs
+++ src/Sitecore.Support.166318/ContentSearch/Azure/Query/LinqToCloudIndex.cs
@@ -1,6 +1,4 @@
-﻿
-
-namespace Sitecore.Support.ContentSearch.Azure.Query
+﻿namespace Sitecore.Support.ContentSearch.Azure.Query
 {
     using System;
     using System.Collections.Generic;
@@ -24,7 +22,9 @@ namespace Sitecore.Support.ContentSearch.Azure.Query
 
         private static readonly MethodInfo miApplyScalarMethods;
 
-        private static readonly MethodInfo miOptimizeQueryExpression;
+        private static MethodInfo miGetOrderByExpression;
+
+        private static MethodInfo miGetFacetExpression;
 
         private readonly ISettings settings;
 
@@ -35,10 +35,11 @@ namespace Sitecore.Support.ContentSearch.Azure.Query
             miApplyScalarMethods = t.GetMethod("ApplyScalarMethods", BindingFlags.Instance | BindingFlags.NonPublic);
             Assert.IsNotNull(miApplyScalarMethods, "miApplyScalarMethods is null...");
 
-            miOptimizeQueryExpression = t.GetMethod("OptimizeQueryExpression",
-                BindingFlags.Instance | BindingFlags.NonPublic);
-            Assert.IsNotNull(miOptimizeQueryExpression, "miOptimizeQueryExpression is null...");
+            miGetOrderByExpression = t.GetMethod("GetOrderByExpression", BindingFlags.Instance | BindingFlags.NonPublic);
+            Assert.IsNotNull(miGetOrderByExpression, "miGetOrderByExpression is null...");
 
+            miGetFacetExpression = t.GetMethod("GetFacetExpression", BindingFlags.Instance | BindingFlags.NonPublic);
+            Assert.IsNotNull(miGetFacetExpression, "miGetFacetExpression is null...");
         }
 
         public LinqToCloudIndex(CloudSearchSearchContext context, IExecutionContext executionContext)
@@ -93,7 +94,7 @@ namespace Sitecore.Support.ContentSearch.Azure.Query
             try
             {
                 result =
-                    (TResult) applyScalarMethodsGenericMethod.Invoke(this, new[] {query, processedResults, totalDoc});
+                    (TResult)applyScalarMethodsGenericMethod.Invoke(this, new[] { query, processedResults, totalDoc });
             }
             catch (TargetInvocationException ex)
             {
@@ -147,7 +148,7 @@ namespace Sitecore.Support.ContentSearch.Azure.Query
                 // Finalize the query expression
                 // append $count=true to find out total found document, but has performance impact;
                 var expression = this.OptimizeQueryExpression(query, searchIndex) + "&$count=true";
-                
+
                 if (settings.GetBoolSetting("Support.ContentSearch.AzureSearch.MatchAllTerms", true))
                 {
                     expression += "&searchMode=all";
@@ -163,48 +164,31 @@ namespace Sitecore.Support.ContentSearch.Azure.Query
                     }
 
                     // TODO Replace this handmade deserialization with SearchResultsDeserializer.
-                    var valueList =
-                        JObject.Parse(results)
-                            .SelectToken("value")
+                    var parsedResult = JObject.Parse(results);
+
+                    var valueList = parsedResult.SelectToken("value")
                             .Select(x => JsonHelper.Deserialize(x.ToString()) as Dictionary<string, object>)
                             .ToList();
-                    if (valueList.Count != 0)
+
+                    totalDoc = parsedResult["@odata.count"].ToObject<int>();
+
+
+                    var skip = this.GetRealSkipValue(query);
+                    var take = this.GetRealTakeValue(query);
+
+                    countDoc = this.CalculateActualCountPerPage(totalDoc, take, skip);
+
+                    var facetData = parsedResult.GetValue("@search.facets");
+                    if (facetData != null)
                     {
-                        totalDoc = JObject.Parse(results)["@odata.count"].ToObject<int>();
-                        countDoc = totalDoc;
-                        var skipFields =
-                            query.Methods.Where(m => m.MethodType == QueryMethodType.Skip)
-                                .Select(m => (SkipMethod) m)
-                                .ToList();
-                        if (skipFields.Any())
-                        {
-                            var start = skipFields.Sum(skipMethod => skipMethod.Count);
-                            countDoc = countDoc - start;
-                        }
-
-                        // If take method is defined, total doc will based on that
-                        var takeFields =
-                            query.Methods.Where(m => m.MethodType == QueryMethodType.Take)
-                                .Select(m => (TakeMethod) m)
-                                .ToList();
-                        if (takeFields.Any())
-                        {
-                            countDoc = takeFields.Sum(takeMethod => takeMethod.Count);
-                            if (valueList.Count < countDoc)
-                            {
-                                countDoc = valueList.Count;
-                            }
-                        }
-
-                        var facetData = JObject.Parse(results).GetValue("@search.facets");
-                        if (facetData != null)
-                        {
-                            facetResult =
-                                JObject.Parse(results).GetValue("@search.facets").ToObject<Dictionary<string, object>>();
-                        }
-
-                        return valueList;
+                        facetResult =
+                            parsedResult.GetValue("@search.facets").ToObject<Dictionary<string, object>>();
                     }
+
+                    // This method is needed to workaround the old behavior of the CloudSearchResults.ElementAt
+                    valueList = this.AdoptResulltForElementAt(query, countDoc, valueList);
+
+                    return valueList;
                 }
                 catch (Exception ex)
                 {
@@ -219,20 +203,200 @@ namespace Sitecore.Support.ContentSearch.Azure.Query
             return new List<Dictionary<string, object>>();
         }
 
-        private string OptimizeQueryExpression(CloudQuery query, CloudSearchProviderIndex index)
+        protected List<Dictionary<string, object>> AdoptResulltForElementAt(CloudQuery query, int actualDocCount, List<Dictionary<string, object>> curResult)
         {
-            return (string)miOptimizeQueryExpression.Invoke(this, new object[] { query, index });
+            var elementAt = this.GetQueryMethods<ElementAtMethod>(query, QueryMethodType.ElementAt).FirstOrDefault();
+            if (elementAt == null)
+            {
+                return curResult;
+            }
+
+            if (curResult.Count == 0)
+            {
+                return curResult;
+            }
+
+            if (curResult.Count > 1)
+            {
+                SearchLog.Log.Warn("SUPPORT Result for ElementAt contains more than 1 documents, only single will be used...");
+            }
+
+            Dictionary<string, object> doc = curResult[0];
+
+            var adoptedResult = new Dictionary<string, object>[elementAt.Index + 1];
+            adoptedResult[elementAt.Index] = doc;
+
+            return adoptedResult.ToList();
         }
 
-        private static SelectMethod GetSelectMethod(CloudQuery compositeQuery)
+        protected virtual int CalculateActualCountPerPage(int total, int? take, int? skip)
         {
-            var selectMethods =
-                compositeQuery.Methods.Where(m => m.MethodType == QueryMethodType.Select)
-                    .Select(m => (SelectMethod) m)
-                    .ToList();
+            // TODO Check case for skip >= total
+            if (total == 0 || take == 0 || skip >= total)
+            {
+                return 0;
+            }
 
-            return selectMethods.Count() == 1 ? selectMethods[0] : null;
+            var rest = total;
+            if (skip.HasValue)
+            {
+                rest -= skip.Value;
+            }
+
+            if (take.HasValue)
+            {
+                rest = Math.Min(take.Value, rest);
+            }
+
+            return Math.Min(rest, this.GetMaxNumberDocumentsInResult());
         }
 
+        protected virtual int GetMaxNumberDocumentsInResult()
+        {
+            // Azure Search Service limitation
+            const int AzureSearchMaxDocuments = 1000;
+
+            var maxDocumentsPerPage = Sitecore.Configuration.Settings.GetIntSetting("ContentSearch.AzureSearch.SearchMaxResults", -1);
+
+            if (maxDocumentsPerPage == -1)
+            {
+                maxDocumentsPerPage = Sitecore.Configuration.Settings.GetIntSetting("ContentSearch.SearchMaxResults", int.MaxValue);
+            }
+
+            return Math.Min(maxDocumentsPerPage, AzureSearchMaxDocuments);
+        }
+
+        protected int? GetRealTakeValue(CloudQuery query)
+        {
+            var takeMethods = this.GetQueryMethods<TakeMethod>(query, QueryMethodType.Take).ToList();
+
+            var totalTake = takeMethods.Count > 0 ? (int?)takeMethods.Sum(takeMethod => takeMethod.Count) : null;
+
+            // MSDN descrption says if value < 0 return all values 
+            return totalTake < 0 ? null : totalTake;
+        }
+
+        protected int? GetRealSkipValue(CloudQuery query)
+        {
+            var skipMethods = this.GetQueryMethods<SkipMethod>(query, QueryMethodType.Skip).ToList();
+
+            var totalSkip = skipMethods.Count > 0 ? (int?)skipMethods.Sum(skipMethod => skipMethod.Count) : null;
+
+            // MSDN descrption says if value < 0, elements must not be skipped
+            return totalSkip < 0 ? 0 : totalSkip;
+        }
+
+        protected virtual int? GetSkipParameterValue(CloudQuery query, CloudSearchProviderIndex index)
+        {
+            int? skipValue = this.GetRealSkipValue(query);
+
+            var elementAt = this.GetQueryMethods<ElementAtMethod>(query, QueryMethodType.ElementAt).FirstOrDefault();
+
+            if (elementAt != null)
+            {
+                if (skipValue == null)
+                {
+                    skipValue = elementAt.Index;
+                }
+                else
+                {
+                    skipValue += elementAt.Index;
+                }
+            }
+
+            return skipValue;
+        }
+
+        protected virtual int GetTopParameterValue(CloudQuery query, CloudSearchProviderIndex index)
+        {
+            // Take method 
+            int? topValue = this.GetRealTakeValue(query);
+
+            // Methods Executors
+            var countMethod = this.GetQueryMethods<CountMethod>(query, QueryMethodType.Count).FirstOrDefault();
+
+            if (countMethod != null)
+            {
+                topValue = 0;
+            }
+
+            var getFacetsMethod = this.GetQueryMethods<GetFacetsMethod>(query, QueryMethodType.GetFacets).FirstOrDefault();
+
+            if (getFacetsMethod != null)
+            {
+                topValue = 0;
+            }
+
+            var firstMethod = this.GetQueryMethods<FirstMethod>(query, QueryMethodType.First).FirstOrDefault();
+
+            if (firstMethod != null)
+            {
+                topValue = 1;
+            }
+
+            var singleMethod = this.GetQueryMethods<SingleMethod>(query, QueryMethodType.Single).FirstOrDefault();
+
+            if (singleMethod != null)
+            {
+                topValue = 2;
+            }
+
+            var anyMethod = this.GetQueryMethods<AnyMethod>(query, QueryMethodType.Any).FirstOrDefault();
+
+            if (anyMethod != null)
+            {
+                topValue = 1;
+            }
+
+            var elementAtMethod = this.GetQueryMethods<ElementAtMethod>(query, QueryMethodType.ElementAt).FirstOrDefault();
+
+            if (elementAtMethod != null)
+            {
+                // topValue = 0 for cases .Take(3).ElementAt(4); where we know result is empty
+                topValue = topValue <= elementAtMethod.Index ? 0 : 1;
+            }
+
+            return topValue ?? this.GetMaxNumberDocumentsInResult();
+        }
+
+
+        protected virtual SelectMethod GetSelectMethod(CloudQuery compositeQuery)
+        {
+            return this.GetQueryMethods<SelectMethod>(compositeQuery, QueryMethodType.Select).SingleOrDefault();
+        }
+
+        protected IEnumerable<TQMethod> GetQueryMethods<TQMethod>(CloudQuery compositeQuery, QueryMethodType methodType) where TQMethod : QueryMethod
+        {
+            return compositeQuery.Methods.Where(m => m.MethodType == methodType).Select(m => (TQMethod)m);
+        }
+
+        protected virtual string GetOrderByExpression(CloudQuery query, CloudSearchProviderIndex index)
+        {
+            return (string)miGetOrderByExpression.Invoke(this, new object[] { query, index });
+        }
+
+        protected virtual string GetFacetExpression(CloudQuery query, CloudSearchProviderIndex index)
+        {
+            return (string)miGetFacetExpression.Invoke(this, new object[] { query, index });
+        }
+
+
+        protected virtual string OptimizeQueryExpression(CloudQuery query, CloudSearchProviderIndex index)
+        {
+            var expression = query.Expression;
+
+            var skip = this.GetSkipParameterValue(query, index);
+            string skipParameter = skip == null ? string.Empty : $"&$skip={skip}";
+
+            var top = this.GetTopParameterValue(query, index);
+
+            var facetExpression = this.GetFacetExpression(query, index);
+
+            expression = Sitecore.ContentSearch.Azure.Query.CloudQueryBuilder.Merge(expression, facetExpression, "and", Sitecore.ContentSearch.Azure.Query.CloudQueryBuilder.ShouldWrap.Both);
+
+            var orderByExpression = this.GetOrderByExpression(query, index);
+
+            return $"{expression}{orderByExpression}&$top={top}{skipParameter}";
+        }
     }
 }
